045c38f41dadb22b0859ffc0facf110169a30874
Sarah Ruana <ruana.s@northeastern.edu>
2026-04-13 23:46:21 -0400

    finished writing the graphs to compare the agents, + wrote some tests for the random agent

--- evaluation/AgentComparison.py
+++ evaluation/AgentComparison.py
@@ -28,7 +28,7 @@ class AgentComparison:
 
         while not board.allSunk():
             unsunk = [ship for ship in board.ships if not ship.isSunk()]
-            row, col = agent.chooseAttack(board, unsunk)
+            row, col = agent.chooseAttack(board,unsunk)
             
             result = board.attacked(row, col)
             shots += 1
@@ -49,7 +49,7 @@ class AgentComparison:
     @param agent represents the agent being tested
     @param numGames (int) represents how many games that get played
     '''
-    def runTrials(eval, agent, numGames = 500):
+    def runTrials(eval, agent, numGames):
         allShots = []
         allHitRates = []
 
@@ -79,3 +79,84 @@ class AgentComparison:
         player.placeShipsRand()
         
         return player.board
+
+
+    '''
+    Runs all of the trials and displays/saves the three graphs
+    '''
+    def showResults(eval):
+        monte = MonteCarloAgent()
+        rand = RandomAgent()
+
+        monteResults = eval.runTrials(monte, 500)
+        randResults = eval.runTrials(rand, 500)
+
+        eval.showShotResults(monteResults, randResults)
+        eval.showAvgs(monteResults, randResults)
+        eval.showShotsPerGame(monteResults, randResults, 500)
+
+    
+    '''
+    Shows a histogram of the # of shots to win for both agents
+    @param monteResults (dict) represents the results from the Monte Carlo trials
+    @param randResults (dict) represent the results from the Randome trials
+    '''
+    def showShotResults(eval, monteResults, randResults):
+        
+        plt.figure()
+        plt.hist(monteResults["allShots"], bins=20, alpha=0.6, label="Monte Carlo", color="blue")
+        plt.hist(randResults["allShots"], bins=20, alpha=0.6, label="Random", color="red")
+        
+        plt.title("Number of Shots to Win")
+        plt.xlabel("Shots")
+        plt.ylabel("Games")
+        plt.legend()
+        
+        plt.savefig("shotResults.png")
+        plt.show()
+
+    '''
+    Shows a bar chart for both the avg shots and the avg hit rate for both agents
+    @param monteResults (dict) represents the results from the Monte Carlo trials
+    @param randResults (dict) represent the results from the Randome trials
+    '''
+    def showAvgs(eval, monteResults, randResults):
+        agents = ["Monte Carlo", "Random"]
+
+        plt.figure()
+        plt.bar(agents, [monteResults["avgShots"], randResults["avgShots"]], 
+                color=["blue", "red"], alpha=0.6)
+        
+        plt.title("Avg Shots to Win")
+        plt.ylabel("Shots")
+        plt.savefig("avgShots.png")
+        plt.show()
+
+        plt.figure()
+        plt.bar(agents, [monteResults["avgHitRate"] * 100, randResults["avgHitRate"] * 100], 
+                color=["blue", "red"], alpha=0.6)
+        plt.title("Avg Hit Rate")
+        plt.ylabel("Hit Rate (%)")
+        plt.savefig("avgHitRate.png")
+        plt.show()
+
+    '''
+    Shows a line graph that represnts the shots per game over time for both agents
+    @param monteResults (dict) represents the results from the Monte Carlo trials
+    @param randResults (dict) represent the results from the Randome trials
+    @param num (int) represents how many games were played
+    '''
+    def showShotsPerGame(eval, monteResults, randResults, num):
+        numGames = list(range(1, num + 1))
+
+        plt.figure()
+        plt.plot(numGames, monteResults["allShots"], alpha=0.5, label="Monte Carlo", color="blue")
+        plt.plot(numGames, randResults["allShots"], alpha=0.5, label="Random", color="red")
+        
+        plt.title("Shots Per Game")
+        plt.xlabel("Game")
+        plt.ylabel("Shots")
+        plt.legend()
+        plt.savefig("shotsPerGame")
+        plt.show()
+    
--- evaluation/RandomAgent.py
+++ evaluation/RandomAgent.py
@@ -12,14 +12,15 @@ class RandomAgent:
     Picks a random open cell to attack
     Returns (tuple[int, int]) representing the chosen (row, col)
     @param opponentBoard (Board) represents the opponent's board
+    @param unsunk is just so that the random agent and the monte agent have the same signature
     '''
-    def chooseAttack(agent, opponentBoard):
+    def chooseAttack(agent, opponentBoard, unsunk):
         avail = []
 
         for row in range(opponentBoard.size):
             for col in range(opponentBoard.size):
 
                 if (row, col) not in opponentBoard.attacks:
-                    avail.append((row,col))
+                    avail.append((row, col))
 
         return random.choice(avail)
